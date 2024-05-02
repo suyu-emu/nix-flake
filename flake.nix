4253231a9349a54ec9899e82a784e2aa30571085
VictorGamerLOL <hackandgo3@gmail.com>
2024-05-02 19:09:20 +0100

    reworked flake to allow for more platforms
    It should work on aarch64 now. However I do not own such machine so it is untested.

--- flake.nix
+++ flake.nix
@@ -3,21 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # todo: get a mac
+    # todo: wait for suyu to get good on MacOS
+    systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = 
+  outputs = { self, nixpkgs, systems, ... }: 
+  let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in
+  {
+    packages = eachSystem (system: 
     let
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config = {allowUnfree = true;};
+        system = "${system}";
+        config = {allowUnfree = true;}; # cuda
       };
-      suyu-deps = pkgs.kdePackages // {inherit (self.packages.x86_64-linux) nx_tzdb compat-list;};
+
+      # Qt6 packages have been moved to kdePackages so we grab them all here so they are not
+      # confused with their Qt5 counterparts
+      suyu-deps = pkgs.kdePackages // {inherit (self.packages.${system}) nx_tzdb compat-list;};
     in {
-      default = self.packages.x86_64-linux.suyu;
+      default = self.packages.${system}.suyu;
       nx_tzdb = pkgs.callPackage ./suyu/nx_tzdb.nix {};
       compat-list = pkgs.callPackage ./suyu/compat-list.nix {};
       suyu = pkgs.callPackage ./suyu/suyu.nix suyu-deps;
-    };
+    }
+    );
   };
 }
